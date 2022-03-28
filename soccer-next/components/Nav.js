a21975eaa44f8d70e89a02c68fcc9a3071e7cebb
BeautyKim <kima2012114@gmail.com>
2022-03-28 17:35:58 +0900

    first commit

---
+++ soccer-next/components/Nav.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import styles from "../styles/Nav.module.css";
+
+export default function Nav(){
+    return(
+        <nav className={styles.nav}>
+            <ul>
+                <li className={styles.li}> <Link href="/">Home</Link></li>
+                <li className={styles.li}> <Link href="/bmi">Bmi</Link></li>
+                <li className={styles.li}> <Link href="/calc">Calc</Link></li>
+                <li className={styles.li}> <Link href="/grade">Grade</Link></li>
+            </ul>
+        </nav>
+    );
+}
---
+++ soccer-next/pages/basic/bmi.js
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+
+export default function Bmi(){
+    const[inputs, setInputs] = useState({})
+    const[result, setResult] = useState('')
+    const{ name, height, weight}= inputs;
+
+     const handleChange=(e)=>{
+        e.preventDefault()
+         const{value, name} = e.target;
+         setInputs({
+             ...inputs, [name]: value
+         })
+     }
+
+        const handleClick = (e)=>{
+            e.preventDefault()
+            const bmiRequest = {name, weight, height}
+
+        }
+        
+    return(<>
+        <form>
+        <h1>BMI 폼</h1>
+    
+
+    <div>
+        <label htmlFor=""><b>name</b></label>
+            <input type="text" name="name" onChange={handleChange}/><br/>
+
+        <label htmlFor=""><b>height</b></label>
+            <input type="text" name="height" onChange={handleChange}/><br/>
+
+        <label htmlFor=""><b>weight</b></label>
+            <input type="text" name="weight" onChange={handleChange}/><br/>
+
+            <button onClick={handleClick}>BMI체크</button>
+
+
+    </div>
+    </form>
+    <div>결과: {result}</div>
+
+    </>)
+}
